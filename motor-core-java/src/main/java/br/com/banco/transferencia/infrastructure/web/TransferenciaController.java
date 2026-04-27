bc36557dcbff8b374b4b1c97e8b69a4143d40d22
yunkbaza <allanbamirati@live.com>
2026-04-27 19:32:37 -0300

    Redis implementado

--- motor-core-java/src/main/java/br/com/banco/transferencia/infrastructure/web/TransferenciaController.java
+++ motor-core-java/src/main/java/br/com/banco/transferencia/infrastructure/web/TransferenciaController.java
@@ -3,10 +3,12 @@ package br.com.banco.transferencia.infrastructure.web;
 import br.com.banco.transferencia.application.ports.out.TransacaoRepositoryPort;
 import br.com.banco.transferencia.application.usecases.RealizarTransferenciaUseCase;
 import br.com.banco.transferencia.domain.Transacao;
+import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
 import java.util.List;
 
 @RestController
@@ -15,33 +17,49 @@ public class TransferenciaController {
 
     private final RealizarTransferenciaUseCase realizarTransferenciaUseCase;
     private final TransacaoRepositoryPort transacaoRepositoryPort;
+    private final StringRedisTemplate redisTemplate; // <-- SÊNIOR: Injeção do Redis em Memória
 
-    public TransferenciaController(RealizarTransferenciaUseCase realizarTransferenciaUseCase, TransacaoRepositoryPort transacaoRepositoryPort) {
+    public TransferenciaController(RealizarTransferenciaUseCase realizarTransferenciaUseCase,
+                                   TransacaoRepositoryPort transacaoRepositoryPort,
+                                   StringRedisTemplate redisTemplate) {
         this.realizarTransferenciaUseCase = realizarTransferenciaUseCase;
         this.transacaoRepositoryPort = transacaoRepositoryPort;
+        this.redisTemplate = redisTemplate;
     }
 
     @PostMapping
-    public ResponseEntity<TransacaoResponse> transferir(@RequestBody TransferenciaRequest request) {
-        Transacao transacao = realizarTransferenciaUseCase.executar(
-                request.contaOrigemId(),
-                request.contaDestinoId(),
-                request.valor()
-        );
+    public ResponseEntity<?> transferir(
+            @RequestHeader(value = "Idempotency-Key", required = true) String idempotencyKey, // <-- SÊNIOR: A chave única do Android
+            @RequestBody TransferenciaRequest request) {
 
-        // Mapeamento limpo usando Factory Method
-        return ResponseEntity.status(HttpStatus.CREATED).body(TransacaoResponse.fromDomain(transacao));
+        // 1. Verificação de Idempotência no Redis (Tempo de resposta < 2ms)
+        String redisKey = "PIX_IDEMP:" + idempotencyKey;
+        Boolean isNovoPedido = redisTemplate.opsForValue().setIfAbsent(redisKey, "PROCESSANDO", Duration.ofHours(24));
+
+        if (Boolean.FALSE.equals(isNovoPedido)) {
+            // Se já existe no Redis, é um duplo clique ou ataque de repetição
+            return ResponseEntity.status(HttpStatus.CONFLICT)
+                    .body("Transação duplicada bloqueada pelo motor de idempotência.");
+        }
+
+        try {
+            // 2. Executa a transferência normal no PostgreSQL
+            Transacao transacao = realizarTransferenciaUseCase.executar(
+                    request.contaOrigemId(), request.contaDestinoId(), request.valor()
+            );
+            return ResponseEntity.status(HttpStatus.CREATED).body(TransacaoResponse.fromDomain(transacao));
+
+        } catch (Exception e) {
+            // Se a regra de negócio falhar (ex: Saldo Insuficiente), libertamos a chave para o utilizador tentar de novo
+            redisTemplate.delete(redisKey);
+            throw e;
+        }
     }
 
     @GetMapping
     public ResponseEntity<List<TransacaoResponse>> listarTodas() {
-        // DICA SÊNIOR: Em produção, utilize Pageable (org.springframework.data.domain.Pageable)
-        // para evitar OutOfMemory (OOM) em contas com muitas transações.
         List<TransacaoResponse> extrato = transacaoRepositoryPort.buscarTodas()
-                .stream()
-                .map(TransacaoResponse::fromDomain)
-                .toList();
-
+                .stream().map(TransacaoResponse::fromDomain).toList();
         return ResponseEntity.ok(extrato);
     }
 }
